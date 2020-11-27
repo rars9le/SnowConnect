8918b970dd507bd0ac3b6fd770e9e07774c69fd9
lenon <le9240@gmail.com>
2020-11-27 17:48:37 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:  "管理者",
+Faker::Config.locale = :ja
+
+# 管理者ユーザー作成
+User.create!(name:  "AdminUser",
   email: "admin@example.com",
   password:  "12345678",
   password_confirmation: "12345678",
   admin: true)
 
+# ゲストユーザー作成
 User.create!(name:  "GuestUser",
   email: "guest@example.com",
   password:  "12345678",
   password_confirmation: "12345678",
   guest: true)
 
+# ユーザー作成
 1.upto(49) do |i|
   name  = Faker::Name.name
   email = "sample-#{i}@example.com"
@@ -28,4 +33,49 @@ User.create!(name:  "GuestUser",
                 password_confirmation: password,
                 confirmed_at: Time.zone.now,
                 confirmation_sent_at: Time.zone.now)
+end
+
+# ユーザーアバター生成
+users = User.order(:id).take(10)
+users.each_with_index do |user, i|
+  user.avator = open("#{Rails.root}/db/fixtures/avator/avator-#{i + 1}.jpg")
+  user.save
+end
+
+# Post作成
+i = 0
+users = User.order(:id).take(3)
+
+users.each do
+  1.upto(7) do |j|
+    j = ((i * 7) + j)
+    image = open("#{Rails.root}/db/fixtures/post/post-#{j}.jpg")
+    content = Faker::Lorem.sentence
+
+    users[i].posts.create!(
+      image: image,
+      content: content,
+      created_at: i.zero? ? Time.zone.now : rand(Time.zone.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day)
+    )
+  end
+  i += 1
+end
+
+# お気に入りデータ作成
+users = User.order(:id).take(5)
+posts = Post.order(:id).take(10)
+users.each do |user|
+  posts.each do |post|
+    user.favorite(post) unless user.id == post.user_id
+  end
+end
+
+# リレーションシップ
+users = User.all
+user_array = [users.find(1), users.find(2), users.find(3), users.find(4), users.find(5)]
+user_array.each_with_index do |user, index|
+  following = users[index + 2..index + 40]
+  followers = users[index + 3..index + 30]
+  following.each { |followed| user.follow(followed) }
+  followers.each { |follower| follower.follow(user) }
 end
