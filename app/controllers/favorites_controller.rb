8d0e2af0d431d864d5e9e140a76c57f31f443ba4
lenon <le9240@gmail.com>
2020-12-21 23:46:41 +0900

    お気に入り削除ajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,13 @@ class FavoritesController < ApplicationController
   def destroy
     post = Post.find(params[:post_id])
     current_user.unfavorite(post)
-    flash[:success] = 'お気に入りを解除しました。'
-    redirect_back(fallback_location: root_path)
+
+    respond_to do |format|
+      format.html { redirect_to request.referer || root_url }
+      format.js
+    end
+
+    # flash[:success] = 'お気に入りを解除しました。'
+    # redirect_back(fallback_location: root_path)
   end
 end
